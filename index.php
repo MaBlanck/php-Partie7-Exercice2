bb1b1f02a92826a4244bb9ebba3b8efe568970d8
Max <maxblanck@outlook.fr>
2020-12-09 15:48:21 +0100

    php Partie 7  Exercice 2

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 2 Partie 7</title>
+</head>
+
+<body>
+    <h1>Exercice 2 Partie 7</h1>
+    <form action="user.php" method="post">
+        <fieldset>
+            <legend>Formulaire</legend>
+            <label for="lastname">Nom</label>
+            <input type="text">
+            <label for="firstname">Prénom</label>
+            <input type="text">
+        </fieldset>
+    </form>
+</body>
+
+</html>
